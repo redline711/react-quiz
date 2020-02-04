bfc36614abf9552c55e4d80628a143bf74e01e9c
Vova Kalinichenko <vova.kalinichenko@dreamhost.com>
2020-02-04 18:46:11 +0200

    6.14 Menu toggle button

---
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './MenuToggle.module.css'
+
+const MenuToggle = props => {
+	const cls = [classes.MenuToggle]
+	if (props.isOpen) {
+		cls.push(classes.open)
+	}
+	return (
+			<i className={cls.join(' ')} onClick={props.onToggle}/>
+	)
+}
+
+export default MenuToggle
